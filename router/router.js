f9f91ea105419593b3cfc0f4930cdd524b640633
SahilSheorn <sahilks0317@gmail.com>
2024-06-06 11:40:22 +0530

    Expensives Apis

--- router/router.js
+++ router/router.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const {
-  addFields,
-  getData,
-  deleteCategory,
-  updateFields,
-} = require("../controller/productController");
-const { addBill } = require("../controller/billController");
+const { addFields, getData, deleteCategory, updateFields }
+  = require("../controller/productController");
+const { addExpens, getExpens, updateExpens, deleteExpens }
+  = require("../controller/expensisesController");
+const { addBill, getBillData } = require("../controller/billController");
 const { getCategoryData, addCategory } = require("../controller/categoryController");
 
 router.post("/category", addFields);
 router.get("/getdata", getData);
 router.delete("/category/:id", deleteCategory);
 router.put("/category/:id", updateFields);
-router.get("/getdata", getData); 
+router.get("/getdata", getData);
 router.post("/bill", addBill);
-router.get("/getCategoryData", getCategoryData); 
+router.get("/bills", getBillData);
+router.get("/getCategoryData", getCategoryData);
 router.post("/addcategory", addCategory);
 
-
-
+router.post("/expenses", addExpens);
+router.get("/expenses", getExpens);
+router.put("/expens/:id", updateExpens);
+router.delete("/expens/:id", deleteExpens);
 
 module.exports = router;
