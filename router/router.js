b53229ee6c6f7c8e24c4a5c4aab89d7e4649a087
SahilSheorn <sahilks0317@gmail.com>
2024-06-10 15:33:25 +0530

    Category

--- router/router.js
+++ router/router.js
@@ -6,21 +6,22 @@ const {
   getProducts,
   deleteProduct,
   updateProduct,
-  } = require("../controller/productController");
+} = require("../controller/productController");
 const { addBill, getBillData, addTableBill } = require("../controller/billController");
-  
-  const { getProductData, addCategory, getCategories, getAllProducts } = require("../controller/categoryController");
-  const { addExpens, getExpens, updateExpens, deleteExpens }
+
+const { addNewCategories, getNewCategories, updateNewCategories, deleteNewCategories } = require('../controller/categoriesController')
+const { getProductData, addCategory, getCategories, getAllProducts } = require("../controller/categoryController");
+const { addExpens, getExpens, updateExpens, deleteExpens }
   = require("../controller/expensisesController");
-  const { registerUser, loginUser } = require("../controller/userController");
-  // const auth = require("../middleware/auth");
+const { registerUser, loginUser } = require("../controller/userController");
+// const auth = require("../middleware/auth");
 
 const {
   addStructureFields,
   getStructureData,
   deleteStructure,
 } = require("../controller/structureController");
-  
+
 router.post("/product", addProduct);
 router.get("/get-products", getProducts);
 router.put("/product/:id", updateProduct);
@@ -29,15 +30,20 @@ router.delete("/product/:id", deleteProduct);
 router.post("/bill", addBill);
 router.get("/bills", getBillData);
 
-router.get("/get-product-data", getProductData); 
+router.get("/get-product-data", getProductData);
 router.post("/addcategory", addCategory);
-router.get("/categories", getCategories); 
-router.get("/products", getAllProducts); 
+router.get("/categories", getCategories);
+router.get("/products", getAllProducts);
 
 router.post("/expenses", addExpens);
 router.get("/expenses", getExpens);
 router.put("/expens/:id", updateExpens);
 router.delete("/expens/:id", deleteExpens);
+
+router.post("/newcategories", addNewCategories);
+router.get("/newcategories", getNewCategories);
+router.put("/newcategories/:id", updateNewCategories);
+router.delete("/newcategories/:id", deleteNewCategories);
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
