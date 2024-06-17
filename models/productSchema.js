e8ebdbadefc3a810066b389d48fae42b66274807
vinunarwal <vinunarwal3@gmail.com>
2024-06-17 15:01:58 +0530

    ..

--- models/productSchema.js
+++ models/productSchema.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  productid: String,
+  productid: { type: Number, required: true },
   userId: { type: String, required: true },
   productName: String,
   type: String,
   category: String,
   unit: String,
   stock: String,
-  price: String,
+  price: Number,
   description: String,
   timestamp: {
     type: Date,
