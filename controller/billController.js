dfa4c3e940143cfa2dd7b40a7785e15986096cd4
vinunarwal <vinunarwal3@gmail.com>
2024-06-27 21:50:03 +0530

    ..

--- controller/billController.js
+++ controller/billController.js
@@ -4,10 +4,10 @@ require("dotenv").config();
 const addBill = async (req, res) => {
   const userId = req.params.userId;
   try {
-    const { productName, mobile, section, index, orderItems, totalAmount } = req.body;
+    const { name, mobile, section, index, orderItems, totalAmount } = req.body;
 
     const newBill = new Bill({
-      productName,
+      name,
       userId,
       mobile,
       section,
@@ -16,7 +16,7 @@ const addBill = async (req, res) => {
       totalAmount,
     });
 
-    await newBill.save();
+    await newBill.save(); 
 
     res.status(201).json({
       message: "Order placed successfully",
